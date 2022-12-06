7ca161e5f26a83294042f2d222de688fbece1543
aendirr <118621878+Aendirr@users.noreply.github.com>
2022-12-06 18:41:03 +0300

    Add files via upload

---
+++ Codes.py
@@ -0,0 +1,47 @@
+from pytube import YouTube
+yt = YouTube("https://www.youtube.com/watch?v=n06H7OcPd-g")
+yt = yt.get('mp4', '720p')
+yt.download('/path/to/download/directory')
+
+
+YouTube('video_url').streams.first().download('save_path')
+
+
+YouTube('video_url').streams.first().download('C:\\Users\\username\\save_path')
+
+
+
+
+
+
+from __future__ import unicode_literals
+import youtube_dl
+
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
+
+
+
+from pytube import YouTube
+yt = YouTube("https://www.youtube.com/watch?v=n06H7OcPd-g")
+yt = yt.get('mp4', '720p')
+yt.download('/path/to/download/directory')
+
+
+YouTube('video_url').streams.first().download('save_path')
+
+
+YouTube('video_url').streams.first().download('C:\\Users\\username\\save_path')
+
+
+
+
+from __future__ import unicode_literals
+import youtube_dl
+
+ydl_opts = {}
+with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+    ydl.download(['https://www.youtube.com/watch?v=BaW_jenozKc'])
+
+
